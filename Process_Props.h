9624ff05b0d9809195484da80f71c1af3c09654c
victor <vnault@haverford.edu>
2024-02-17 13:56:19 -0800

    More work on process table linked list

--- Process_Props.h
+++ Process_Props.h
@@ -1,9 +1,27 @@
+#include <unistd.h>
+
 #ifndef PROCESS_PROPS_H
 #define PROCESS_PROPS_H
 
 typedef struct {
     pid_t pid;
+    int in_foreground;
+    int is_suspended;
+    char * starting_command;
 } Process_Props;
+
+Process_Props * newProcess_Props(pid_t pid, int in_foreground, char * starting_command);
+
+// Getters
+pid_t get_pid(Process_Props * input);
+int get_in_foreground(Process_Props * input);
+int get_is_suspended(Process_Props * input);
+char * get_starting_command(Process_Props * input);
+
+// Setters
+void set_in_foreground(Process_Props * input, int to_set);
+void set_is_suspended(Process_Props * input, int to_set);
+
 
 
 #endif
