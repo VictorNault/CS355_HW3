8d7ae03f58ab43b24d5c16a77b15ac3bb1e378c6
victor <vnault@haverford.edu>
2024-02-21 09:40:53 -0800

    added job id flag to struct

--- Process_Props.h
+++ Process_Props.h
@@ -7,15 +7,17 @@ typedef struct {
     pid_t pid;
     int in_foreground;
     int is_suspended;
+    int job_id;
     char * starting_command;
 } Process_Props;
 
-Process_Props * newProcess_Props(pid_t pid, int in_foreground, char * starting_command);
+Process_Props * newProcess_Props(pid_t pid, int in_foreground, int job_id, char * starting_command);
 
 // Getters
 pid_t get_pid(Process_Props * input);
 int get_in_foreground(Process_Props * input);
 int get_is_suspended(Process_Props * input);
+int get_job_id(Process_Props * input);
 char * get_starting_command(Process_Props * input);
 
 // Setters
