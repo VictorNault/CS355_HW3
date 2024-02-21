16c6be818b85683ea0cce7f5f93b88513477108c
victor <vnault@haverford.edu>
2024-02-21 14:26:16 -0800

    added termios to node data struct

--- Process_Props.h
+++ Process_Props.h
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <termios.h>
 
 #ifndef PROCESS_PROPS_H
 #define PROCESS_PROPS_H
@@ -9,9 +10,10 @@ typedef struct {
     int is_suspended;
     int job_id;
     char * starting_command;
+    struct termios process_termios;
 } Process_Props;
 
-Process_Props * newProcess_Props(pid_t pid, int in_foreground, int job_id, char * starting_command);
+Process_Props * newProcess_Props(pid_t pid, int in_foreground, int job_id, char * starting_command, struct termios process_termios);
 
 // Getters
 pid_t get_pid(Process_Props * input);
@@ -19,10 +21,12 @@ int get_in_foreground(Process_Props * input);
 int get_is_suspended(Process_Props * input);
 int get_job_id(Process_Props * input);
 char * get_starting_command(Process_Props * input);
+struct termios get_process_termios(Process_Props * input);
 
 // Setters
 void set_in_foreground(Process_Props * input, int to_set);
 void set_is_suspended(Process_Props * input, int to_set);
+void set_process_termios(Process_Props * input, struct termios to_set);
 
 
 
